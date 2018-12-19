e5cf3d8798ec01e029b6c2c1a4245fe922b074b7
Sami <sami.heddid@efrei.net>
2018-12-19 15:43:11 +0100

    jquery + jonction table/question

--- load_messages.php
+++ load_messages.php
@@ -1,6 +1,8 @@
 <?php 
+ session_start();
+
 $bdd = new PDO ('mysql:host=localhost;dbname=munaziraMembre', 'root', 'root'); 
-    $allmsg = $bdd -> query('SELECT * FROM chat ORDER BY id DESC LIMIT 0,6'); // Pour afficher les messages les plus récent en premier dans la limite de 6 messages
+    $allmsg = $bdd -> query('SELECT * FROM chat'.$_SESSION['question'].' ORDER BY id DESC LIMIT 0,6'); // Pour afficher les messages les plus récent en premier dans la limite de 6 messages
     while($msg = $allmsg -> fetch())
     {
     ?>
