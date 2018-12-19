0989bd5daed2c1595d41cb4856d0dd9152492548
Maria <maria.sadek@efrei.net>
2018-12-19 10:27:56 +0100

    guide utilisateur

---
+++ savoirPlus.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> <!-- Pour indiquer qu'il s'agit d'une page web -->
+<html>
+<head>
+<link rel="stylesheet" href="css/lirePlus.css" />
+<title>Maria</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<h1>MUNAZIRA</h1><!--h1,...,h6 pour les titres de differentes tailles-->
+<h2>Munazira qu'est-ce que c'est ?</h2>
+<p> <em>Munazira</em><!--em:italique mais surtout pour informer l'ordinateur d'un nom important, strong:gras--> ,c'est la nouvelle plateforme de débat où tu peux choisir parmi plusieurs topics, un thème sur lequel tu pourras débattre avec tes amis où avec d'autres utilisateurs.</p>
+<p>Allez directement à :
+<a href="#premier">Règles du Jeu</a>,<!--href="#" pour que le lien corresponde à un mot dans la même page-->
+<a href="#deuxième">Comment s'inscrire ?</a>,
+<a href="#troisième">Nous contacter </a>
+<p>Je vais insérer ici une photo : 
+<img src="css/LogoMunazira.png" alt="logo" class="flottant"/>Donc la je fait genre d'écrire un texte pour pouvoir voir les modification avec les images, pour l'instant j'essaye d'entourer la photo avec le texte, inshalla ca fonctionne.
+</p>
+<ol><!--"ul" sert à créer une liste et "ol" liste numéroté-->
+<li> <h2 id="premier">Règles du Jeu</h2></li><!--"li" sert à créer un point dans une liste-->
+<p>Le but est simple, débattre et partager des opinions sur différents sujets.<br/>Une fois inscrit, tu pourras choisir, parmi une liste proposée, le sujet qui t'interesse. Sept autres personnes vont se joindre au sujet et la partie pourra commencer. Des équipes de 2 seront formées aléatoirement ou de ton choix, ce qui formera deux équipes de 4. <br/>Chaque équipe se verra assigner une couleur: bleu, rouge, jaune et vert. Les rouges et les bleus vont ouvrir le bal, ils débatteront pendant 1 minute 30, chaque joueur peut mettre des commentaires de 150 caractères maximum avec un lapse de temps d'au moins 10 secondes entres chaque commentaire venant de la même personne. Pendant ce temps, les jaunes et les verts observeront le débat et pourront mettre des like sur les commentaires qui leur plaisent. <br/>A la fin de la minute et demi, l'équipe ayant eu le plus de j'aime aura gagné le premier tour. Le deuxième tour se passe de la même manière que le premier avec les jaunes et les verts. <br/>C'est ensuite au 3ème tour de commencer, les deux équipes ayant gagner le premier et le deuxième tour vont débattre ensemble avec pour juges les deux équipes ayant perdu aux deux premiers débats. <br/>Puis le quatrième tour pour les perdants du premier et du deuxième tour vont jouer pour pouvoir définir le classement.<br/> Une fois les 4 parties terminées, un podium est dressé, l'équipe ayant gagné le 3ème débat aura la médaille d'or, l'équipe perdante du 3ème débat et celle gagnate du 4ème auront la medaille d'argent et l'équipe perdante de 4ème débat aura la medaille de bronze.<br/> Les médailles ne seront attribuées qu'une fois la partie entière terminée (c'est à dire après les 4 débats), si un joueur quitte la partie en court de route, même si celui-ci a fini tous ses débats, il ne se verra pas attribué sa medaille/p>
+<li> <h2 id="deuxième">Comment s'inscrire ?</h2></li><!--id="" pour que le lien (plus bas) fasse retourner à cet endroit-->
+    <p>Pour t'inscrire sur le site, remplis le formulaire ici --> <a href="https://openclassrooms.com/fr/courses/1603881-apprenez-a-creer-votre-site-web-avec-html5-et-css3/1604646-creer-des-liens" title="j'ai pas encore fait le formulaire" target=_blank>OpenClassrooms</a></p><!--href pour ajouter un lien (ici externe à munazira), "title" pour faire apparaître une bulle sur le lien et "target" pour ouvrir dans une autre fenêtre-->
+<li> <h2 id="troisième">Nous contacter </h2></li>
+<p>On met nos coordonnés </p>
+</ol>
+</body>
+<html>
